c6e39598d2afca8aa93fcd5947029d9c5c758e3d
Tolibjon <tolibjohn@gmail.com>
2021-10-15 15:01:59 +0500

    Fix linter errors

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -13,48 +13,47 @@ describe Solver do
 
   describe '#factorial' do
     it 'takes parameter and returns a factorial of given number' do
-      expect(@solver.factorial 7).to eql (5040)
+      expect(@solver.factorial(7)).to eql 5040
     end
 
     it 'factorial of 0 is 1' do
-      expect(@solver.factorial 0).to eql (1)
+      expect(@solver.factorial(0)).to eql 1
     end
 
     it 'raise an exception  if a negative integer is given' do
-      expect{@solver.factorial -7}.to raise_error(StandardError)
+      expect { @solver.factorial(-7) }.to raise_error(StandardError)
     end
   end
 
   describe '#reverse' do
     it 'takes string parameter and returns reversed' do
-      expect(@solver.reverse 'Hello').to eql 'olleH'
+      expect(@solver.reverse('Hello')).to eql 'olleH'
     end
 
     it 'takes string parameter and returns reversed' do
-      expect(@solver.reverse 'reverse').to eql 'esrever'
+      expect(@solver.reverse('reverse')).to eql 'esrever'
     end
 
     it 'takes string parameter and returns reversed' do
-      expect(@solver.reverse '12345').to eql '54321'
+      expect(@solver.reverse('12345')).to eql '54321'
     end
   end
 
   describe '#fizzbuzz' do
     it 'takes a integer parameter and returns string following given rules' do
-      expect(@solver.fizzbuzz 6).to eql 'fizz'
+      expect(@solver.fizzbuzz(6)).to eql 'fizz'
     end
 
     it 'takes a integer parameter and returns string following given rules' do
-      expect(@solver.fizzbuzz 10).to eql 'buzz'
+      expect(@solver.fizzbuzz(10)).to eql 'buzz'
     end
 
     it 'takes a integer parameter and returns string following given rules' do
-      expect(@solver.fizzbuzz 15).to eql 'fizzbuzz'
+      expect(@solver.fizzbuzz(15)).to eql 'fizzbuzz'
     end
 
     it 'takes a integer parameter and returns string following given rules' do
-      expect(@solver.fizzbuzz 7).to eql '7'
+      expect(@solver.fizzbuzz(7)).to eql '7'
     end
   end
-
 end
