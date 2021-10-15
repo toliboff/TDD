3860c8aaf1326ab94949d910195e3de3db09e434
Tolibjon <tolibjohn@gmail.com>
2021-10-15 14:33:25 +0500

    Add test cases for raising exception of factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,9 +5,23 @@ describe Solver do
     @solver = Solver.new
   end
 
+  describe '#new' do
+    it 'returns a Solver object' do
+      expect(@solver).to be_an_instance_of(Solver)
+    end
+  end
+
   describe '#factorial' do
-    it 'takes parameters and returns a factorial of given number' do
-      expect(@solver.factorial 3).to eql (6)
+    it 'takes parameter and returns a factorial of given number' do
+      expect(@solver.factorial 7).to eql (5040)
+    end
+
+    it 'factorial of 0 is 1' do
+      expect(@solver.factorial 0).to eql (1)
+    end
+
+    it 'raise an exception  if a negative integer is given' do
+      expect{@solver.factorial -7}.to raise_error(StandardError)
     end
   end
 
