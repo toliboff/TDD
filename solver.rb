a609bb469670517309250b16de611b565267bed9
Hiro Mataba <aganzemataba@gmail.com>
2021-10-15 13:27:19 +0200

    Add  fizzbuz  method

--- solver.rb
+++ solver.rb
@@ -14,4 +14,19 @@ class Solver
   def reverse(word)
     word.reverse
   end
+
+  def fizzbuzz(num)
+    num_divisible_by3 = num.modulo(3).zero?
+    num_divisible_by5 = num.modulo(5).zero?
+
+    if num_divisible_by3 && num_divisible_by5
+      'fizzbuzz'
+    elsif num_divisible_by5
+      'buzz'
+    elsif num_divisible_by3
+      'fizz'
+    else
+      num.to_s
+    end
+  end
 end
